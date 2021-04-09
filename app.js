455a54e5ca73d4cedd832f0bb3bf58f6d148171d
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-04-08 20:47:37 -0500

    app.js is updated with inquirer,fs,util ..

---
+++ app.js
@@ -0,0 +1,10 @@
+// Inside app.js has 2 main functions: main and prompt
+// npm packages and built-in modules
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+// One variable for each object
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
